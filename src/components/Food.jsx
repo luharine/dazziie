77538ea69ebccf0953d21c6a87ee10510e993227
luharine <kattarahul2001@gmail.com>
2022-08-15 18:08:42 +0530

    Added Sidebar.jsx

---
+++ src/components/Food.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Food.css';
+
+export default function Food() {
+  return (
+    <div className='outer'>
+
+        <div className='image'></div>
+
+        <div className='price-outer'>
+        <column>
+            <row>Sashimi</row>
+            <row className='price'> $22</row>         
+        </column>
+        </div>
+            
+            
+        </div>
+  )
+}
---
+++ src/components/Menu.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Menu() {
+  return (
+    <div className='component'>
+      <div className='search'>
+        </div>
+
+      <div className='categories'>
+        </div>
+
+       <div className='cards'>
+          
+        </div> 
+
+        
+
+
+    </div>
+  )
+}
