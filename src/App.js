51400917f4556f03e74b3484e5ca21d72ab548f2
luharine <kattarahul2001@gmail.com>
2022-08-17 18:39:42 +0530

    Fixed App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import Header from './components/Header/Header';
-import logo from './logo.svg';
+
 import Sidebar from './components/Sidebar/Sidebar';
 
 import {Route,Routes} from 'react-router-dom';
-import Food from './components/Food/Food';
+
 import Menu from './components/Menu/Menu';
-import Table from './components/Table/Table';
-import ItemsSold from './components/ItemsSold/ItemsSold';
+
 import Order from './components/Order/Order';
 import './App.css';
 
